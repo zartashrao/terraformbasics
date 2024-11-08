c31861fc30fcc7c8a2938ba21bf330b0873d6584
zartashrao <88138979+zartashrao@users.noreply.github.com>
2024-11-08 17:15:42 +0500

    setup rds module

---
+++ modules/rds/main.tf
@@ -0,0 +1,11 @@
+  identifier           = "rds-terraform"
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = false
+}
